34572a968beaa7da7c7fc726d002607d3841f854
Dima Gerasimov <karlicoss@gmail.com>
2019-07-21 16:18:13 +0100

    move get_todos and set db paths

--- books/kobo/__init__.py
+++ books/kobo/__init__.py
@@ -1,4 +1,15 @@
+import my.books.kobo.kobuddy
+kobuddy.set_databases('/L/backups/kobo')
+
 from my.books.kobo.kobuddy import *
+
+
+def get_todos():
+    def with_todo(ann):
+        if ann is None:
+            ann = ''
+        return 'todo' in ann.lower().split()
+    return by_annotation(with_todo)
 
 
 def test_todos():
