052a500c130a1550a69f29e1cb72bcc5d1ae65f1
Dima Gerasimov <karlicoss@gmail.com>
2019-09-17 21:32:02 +0400

    initial; add stackexchange

---
+++ __init__.py
@@ -0,0 +1,6 @@
+# TODO just eval setup file to populate paths etc?
+# TODO note sure if it would
+import my_configuration
+
+
+paths = my_configuration.paths # type: ignore
---
+++ stackexchange.py
@@ -0,0 +1,14 @@
+from functools import lru_cache
+
+from . import paths
+
+@lru_cache()
+def stexport():
+    from kython import import_file
+    stexport_model = import_file(paths.stexport.repo / 'model.py')
+    return stexport_model
+
+
+def get_data():
+    sources = [max(paths.stexport.export_dir.glob('*.json'))]
+    return stexport().Model(sources).site_model('stackoverflow')
