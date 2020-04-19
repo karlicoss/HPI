0cc4b8ce3a95c8753bbfd61a4467da343e8e8c76
Dima Gerasimov <karlicoss@gmail.com>
2020-04-19 12:35:45 +0100

    hacks to work around python3.6 imports; add set_repo

--- my/cfg.py
+++ my/cfg.py
@@ -17,3 +17,11 @@ After that, you can set config attributes:
 from . import init
 
 import my.config as config
+
+
+def set_repo(name: str, repo):
+    from .init import assign_module
+    from . common import import_from
+
+    module = import_from(repo, name)
+    assign_module('my.config.repos', name, module)
