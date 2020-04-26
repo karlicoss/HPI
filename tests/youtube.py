cd3f2996a3e8c904fd66898da093d13345a06337
Dima Gerasimov <karlicoss@gmail.com>
2020-04-26 12:25:49 +0100

    port tests for takeout from kython

--- tests/youtube.py
+++ tests/youtube.py
@@ -3,9 +3,18 @@
 # these tests would only make sense with some existing data? although some of them would work for everyone..
 # not sure what's a good way of handling this..
 
-from my.media.youtube import get_watched
+from my.media.youtube import get_watched, Watched
 
 
 def test():
     watched = get_watched()
     assert len(watched) > 1000
+
+    from datetime import datetime
+    import pytz
+    w = Watched(
+        url='https://www.youtube.com/watch?v=hTGJfRPLe08',
+        title='Jamie xx - Gosh',
+        when=datetime(year=2018, month=6, day=21, hour=5, minute=48, second=34, tzinfo=pytz.utc),
+    )
+    assert w in watched
