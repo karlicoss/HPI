8d998146e29cd9dcbd424b6835bc2acd2b0937df
Dima Gerasimov <karlicoss@gmail.com>
2020-05-14 22:04:58 +0100

    remove garbage org files, move example config down the hierarchy

--- my/config/__init__.py
+++ my/config/__init__.py
@@ -1,7 +1,7 @@
 # TODO ok, this thing should trigger .cfg import presumably??
 from ..core import init
 
-# TODO maybe, reuse mycfg_template here?
+# TODO maybe, reuse example_config here?
 
 class stub:
     key = 'value'
