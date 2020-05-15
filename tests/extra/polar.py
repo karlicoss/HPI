b2b7eee480678875c0209464e9c21a1febc883a7
Dima Gerasimov <karlicoss@gmail.com>
2020-05-15 07:42:21 +0100

    polar: add test against custom public repos

---
+++ tests/extra/polar.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+
+ROOT = Path(__file__).parent.parent.absolute()
+
+
+import pytest # type: ignore
+
+# todo maybe search fot info.json recursively?
+@pytest.mark.parametrize('dotpolar', [
+    'data/polar/BojanKV_polar/.polar',
+    'data/polar/TheCedarPrince_KnowledgeRepository',
+    'data/polar/coelias_polardocs',
+    'data/polar/warkdarrior_polar-document-repository'
+])
+def test_hpi(dotpolar: str):
+    pdir = Path(ROOT / dotpolar)
+    class user_config:
+        export_dir = pdir
+
+    import my.config
+    setattr(my.config, 'polar', user_config)
+    import sys
+    M = 'my.reading.polar'
+    if M in sys.modules:
+        del sys.modules[M]
+    # TODO maybe set config directly against polar module?
+
+    import my.reading.polar as polar
+    from my.reading.polar import get_entries
+    assert len(list(get_entries())) > 10
