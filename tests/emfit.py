65781dd1528097ce19bfcc1d8a1ea52b7c179892
Dima Gerasimov <karlicoss@gmail.com>
2020-08-21 00:08:55 +0100

    emfit: patch up timezone for correct local sleep time

--- tests/emfit.py
+++ tests/emfit.py
@@ -18,7 +18,6 @@ def test_tz() -> None:
     [s0109] = [s for s in ds if s.date.strftime('%Y%m%d') == '20190109']
     assert s0109.end.strftime('%H:%M') == '06:42'
 
-    # TODO FIXME ugh, it's broken?...
     # summer time, so UTC+1
     [s0411] = [s for s in ds if s.date.strftime('%Y%m%d') == '20190411']
     assert s0411.end.strftime('%H:%M') == '09:30'
