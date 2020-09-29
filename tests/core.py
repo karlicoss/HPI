08ebbd712535209079339f83e29453a3fc061abc
Dima Gerasimov <karlicoss@gmail.com>
2020-09-29 11:01:51 +0100

    core: add tests for core_config

---
+++ tests/core.py
@@ -0,0 +1,14 @@
+'''
+NOTE: Sigh. it's nice to be able to define the tests next to the source code (so it serves as documentation).
+However, if you run 'pytest --pyargs my.core', it detects 'core' package name (because there is no my/__init__.py)
+(see https://docs.pytest.org/en/latest/goodpractices.html#tests-as-part-of-application-code)
+
+This results in relative imports failing (e.g. from ..kython import...).
+
+By using this helper file, pytest can detect the package name properly. A bit meh, but perhaps after kython is moved into the core,
+we can run against the tests in my.core directly.
+
+'''
+
+from my.core.core_config import *
+from my.core.error import *
