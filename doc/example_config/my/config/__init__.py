4a95f9a63e238123260389fa61dd65c5df244dba
Dima Gerasimov <karlicoss@gmail.com>
2020-09-29 19:18:10 +0100

    my.instapaper: add stat; add mypy checks on CI

--- doc/example_config/my/config/__init__.py
+++ doc/example_config/my/config/__init__.py
@@ -2,8 +2,12 @@
 Feel free to remove this if you don't need it/add your own custom settings and use them
 """
 
+from my.core import Paths
+
 class hypothesis:
-    from my.core import Paths
     # expects outputs from https://github.com/karlicoss/hypexport
     # (it's just the standard Hypothes.is export format)
     export_path: Paths = '/path/to/hypothesis/data'
+
+class instapaper:
+    export_path: Paths = ''
