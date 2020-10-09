ed47e98d5c85b3334b3be059ad31b82c6fc0d19c
Dima Gerasimov <karlicoss@gmail.com>
2020-10-09 23:19:13 +0100

    my.body.sleep: integrate with optional temperature data

--- my/body/sleep/common.py
+++ my/body/sleep/common.py
@@ -7,10 +7,26 @@ class Combine:
         self.modules = modules
 
     @cdf
-    def dataframe(self) -> DataFrameT:
+    def dataframe(self, with_temperature: bool=True) -> DataFrameT:
         import pandas as pd # type: ignore
         # todo include 'source'?
         df = pd.concat([m.dataframe() for m in self.modules])
+
+        if with_temperature:
+            from ... import bluemaestro as BM
+            bdf = BM.dataframe()
+            temp = bdf['temp']
+
+            def calc_avg_temperature(row):
+                start = row['sleep_start']
+                end   = row['sleep_end']
+                if pd.isna(start) or pd.isna(end):
+                    return None
+
+                # on no temp data, returns nan, ok
+                return temp[start: end].mean()
+
+            df['avg_temp'] = df.apply(calc_avg_temperature, axis=1)
         return df
 
     def stats(self) -> Stats:
