9aeec433a7889808328537e38f7d92462ab3fe11
Madeline Cameron <maddie@qnzl.co>
2021-04-07 08:30:26 -0400

    my.eightsleep: formatting

--- my/eightsleep.py
+++ my/eightsleep.py
@@ -40,26 +40,26 @@ def dataframe(defensive: bool=True) -> DataFrameT:
         for s in sessions():
             try:
                 d = {
-                    'ts': s['ts'],
-                    'score'      : s['score'],
-                    'stages': pd.DataFrame(s['stages']),
-                    'tossAndTurns': pd.DataFrame(s['tossAndTurns']),
-                    'tempRoomC': pd.DataFrame(s['tempRoomC']),
-                    'tempBedC': pd.DataFrame(s['tempBedC']),
-                    'respiratoryRate': pd.DataFrame(s['respiratoryRate']),
-                    'heartRate': pd.DataFrame(s['heartRate']),
-                    'hrv': pd.DataFrame(s['hrv']),
-                    'rmssd': pd.DataFrame(s['rmssd']),
-                    'stages': pd.DataFrame(s['stages']),
-                    'presenceDuration': s['presenceDuration'] if 'presenceDuration' in s else 0,
-                    'sleepDuration': s['sleepDuration'] if 'sleepDuration' in s else 0,
-                    'deepPercent': s['deepPercent'] if 'deepPercent' in s else 0,
-                    'presenceStart': s['presenceStart'] if 'presenceStart' in s else 0,
-                    'presenceEnd': s['presenceEnd'] if 'presenceEnd' in s else 0,
-                    'sleepStart': s['sleepStart'] if 'sleepStart' in s else 0,
-                    'sleepEnd': s['sleepEnd'] if 'sleepEnd' in s else 0,
+                    'ts'               : s['ts'],
+                    'score'            : s['score'],
+                    'stages'           : pd.DataFrame(s['stages']),
+                    'tossAndTurns'     : pd.DataFrame(s['tossAndTurns']),
+                    'tempRoomC'        : pd.DataFrame(s['tempRoomC']),
+                    'tempBedC'         : pd.DataFrame(s['tempBedC']),
+                    'respiratoryRate'  : pd.DataFrame(s['respiratoryRate']),
+                    'heartRate'        : pd.DataFrame(s['heartRate']),
+                    'hrv'              : pd.DataFrame(s['hrv']),
+                    'rmssd'            : pd.DataFrame(s['rmssd']),
+                    'stages'           : pd.DataFrame(s['stages']),
+                    'presenceDuration' : s['presenceDuration'] if 'presenceDuration' in s else 0,
+                    'sleepDuration'    : s['sleepDuration'] if 'sleepDuration' in s else 0,
+                    'deepPercent'      : s['deepPercent'] if 'deepPercent' in s else 0,
+                    'presenceStart'    : s['presenceStart'] if 'presenceStart' in s else 0,
+                    'presenceEnd'      : s['presenceEnd'] if 'presenceEnd' in s else 0,
+                    'sleepStart'       : s['sleepStart'] if 'sleepStart' in s else 0,
+                    'sleepEnd'         : s['sleepEnd'] if 'sleepEnd' in s else 0,
                     'totalTossAndTurns': s['tnt'] if 'tnt' in s else 0,
-                    'incomplete': s['incomplete'] if 'incomplete' in s else False
+                    'incomplete'       : s['incomplete'] if 'incomplete' in s else False
                 }
             except Exception as e:
                 # TODO use the trace? otherwise str() might be too short..
