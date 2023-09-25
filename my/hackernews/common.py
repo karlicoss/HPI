8addd2d58ab16bc13e2d822c2297181e20aafe78
Dima Gerasimov <karlicoss@gmail.com>
2023-09-25 10:41:36 +0100

    new module: Harmonic app for Hackernews

--- my/hackernews/common.py
+++ my/hackernews/common.py
@@ -1,2 +1,20 @@
+from typing import Protocol
+
+from my.core import datetime_aware, Json
+
+
 def hackernews_link(id: str) -> str:
     return f'https://news.ycombinator.com/item?id={id}'
+
+
+class SavedBase(Protocol):
+    @property
+    def when(self) -> datetime_aware: ...
+    @property
+    def uid(self) -> str: ...
+    @property
+    def url(self) -> str: ...
+    @property
+    def title(self) -> str: ...
+    @property
+    def hackernews_link(self) -> str: ...
