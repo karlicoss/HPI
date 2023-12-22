fa4eb550ae25ffd0778cf72421a92729890f95d6
Dima Gerasimov <karlicoss@gmail.com>
2023-12-22 02:46:41 +0000

    docs: document more experiments with overlays in docs

---
+++ doc/overlays/overlay3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_namespace_packages # type: ignore
+
+
+def main() -> None:
+    pkgs = find_namespace_packages('src')
+    pkg = min(pkgs)
+    setup(
+        name='hpi-overlay3',
+        zip_safe=False,
+        packages=pkgs,
+        package_dir={'': 'src'},
+        package_data={pkg: ['py.typed']},
+    )
+
+
+if __name__ == '__main__':
+    main()
