f8a55f7b37bdb0d7d7b3c7a2aa78982c3c65f1d9
Dima Gerasimov <karlicoss@gmail.com>
2025-01-23 23:16:04 +0000

    general: switch from flat layout to src layout
    this is long overdue and much friendlier to python tooling ecosystem
    
    should be backwards compatible with existing editable installs, with a warning to reinstall properly
    
    see https://github.com/karlicoss/HPI/issues/316

---
+++ src/my/core/internal.py
@@ -0,0 +1,54 @@
+"""
+Utils specific to hpi core, shouldn't really be used by HPI modules
+"""
+from __future__ import annotations
+
+
+def assert_subpackage(name: str) -> None:
+    # can lead to some unexpected issues if you 'import cachew' which being in my/core directory.. so let's protect against it
+    # NOTE: if we use overlay, name can be smth like my.origg.my.core.cachew ...
+    assert name == '__main__' or 'my.core' in name, f'Expected module __name__ ({name}) to be __main__ or start with my.core'
+
+
+def _is_editable(package_name: str) -> bool:
+    import importlib.metadata
+
+    dist = importlib.metadata.distribution(package_name)
+    dist_files = dist.files or []
+    for path in dist_files:
+        if str(path).endswith('.pth'):
+            return True
+    return False
+
+
+def warn_if_not_using_src_layout(path: list[str]) -> None:
+    contains_src = any('/src/my/' in p for p in path)
+    if contains_src:
+        return
+
+    # __package__ won't work because it's gonna be 'my' rather than 'hpi'
+    # seems like it's quite annoying to get distribition name from within the package, so easier to hardcode..
+    distribution_name = 'hpi'
+    try:
+        editable = _is_editable(distribution_name)
+    except:
+        # it would be annoying if this somehow fails during the very first HPI import...
+        # so just make defensive
+        return
+
+    if not editable:
+        # nothing to check
+        return
+
+
+    from . import warnings
+
+    MSG = '''
+    Seems that your HPI is installed as editable and uses flat layout ( my/ directory next to .git folder).
+    This was the case in older HPI versions (pre-20250123), but now src-layout is recommended ( src/my/ directory next to .git folder).
+
+    Reinstall your HPI as editable again via 'pip install --editable /path/to/hpi'.
+    See https://github.com/karlicoss/HPI/issues/316 for more info.
+    '''
+
+    warnings.high(MSG)
--- src/my/tests/common.py
+++ src/my/tests/common.py
@@ -11,8 +11,15 @@ skip_if_not_karlicoss = pytest.mark.skipif(
 )
 
 
+def hpi_repo_root() -> Path:
+    root_dir = Path(__file__).absolute().parent.parent.parent.parent
+    src_dir = root_dir / 'src'
+    assert src_dir.exists(), src_dir
+    return root_dir
+
+
 def testdata() -> Path:
-    d = Path(__file__).absolute().parent.parent.parent / 'testdata'
+    d = hpi_repo_root() / 'testdata'
     assert d.exists(), d
     return d
 
