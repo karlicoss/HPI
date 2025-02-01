cf8b03131fb1b5bf53f58e83f250d5d39fb7d064
Dima Gerasimov <karlicoss@gmail.com>
2025-02-01 13:18:53 +0000

    kompress: get rid of vendorised version
    kompress library dependency was in setup files for a while, and it's a bit of a pain to maintain

--- src/my/kython/kompress.py
+++
@@ -1,5 +0,0 @@
-from my.core import __NOT_HPI_MODULE__, warnings
-
-warnings.high('my.kython.kompress is deprecated, please use "kompress" library directly. See https://github.com/karlicoss/kompress')
-
-from my.core.kompress import *
