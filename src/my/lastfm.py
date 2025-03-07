b9434452cd3fc41c2406fa017fb57ef6f455b596
Dima Gerasimov <karlicoss@gmail.com>
2025-03-07 23:42:46 +0000

    my.lastfm: some cleanup
    - use Protocol based config
    - annotate datetime as datetime_aware
    - switch to dataclass

--- src/my/lastfm.py
+++ src/my/lastfm.py
@@ -2,38 +2,41 @@
 Last.fm scrobbles
 '''
 
+from abc import abstractmethod
+from collections.abc import Iterable, Sequence
 from dataclasses import dataclass
+from datetime import datetime, timezone
+from pathlib import Path
+from typing import Protocol
 
-from my.config import lastfm as user_config
-from my.core import Json, Paths, datetime_aware, get_files, make_logger
+from my.core import Json, Paths, Stats, datetime_aware, get_files, make_logger, stat
+from my.core.cachew import mcachew
 from my.core.json import json_loads
 
 logger = make_logger(__name__)
 
 
-@dataclass
-class lastfm(user_config):
-    """
-    Uses [[https://github.com/karlicoss/lastfm-backup][lastfm-backup]] outputs
-    """
-    export_path: Paths
+class config(Protocol):
+    @property
+    @abstractmethod
+    def export_path(self) -> Paths:
+        """
+        Uses [[https://github.com/karlicoss/lastfm-backup][lastfm-backup]] outputs
+        """
+        raise NotImplementedError
 
 
-from my.core.cfg import make_config
+def make_config() -> config:
+    from my.config import lastfm as user_config
 
-config = make_config(lastfm)
+    class combined_config(user_config, config): ...
 
-
-from collections.abc import Iterable, Sequence
-from datetime import datetime, timezone
-from pathlib import Path
-from typing import NamedTuple
-
-from my.core.cachew import mcachew
+    return combined_config()
 
 
 def inputs() -> Sequence[Path]:
-    return get_files(config.export_path)
+    cfg = make_config()
+    return get_files(cfg.export_path)
 
 
 # TODO memoised properties?
@@ -42,12 +45,12 @@ def inputs() -> Sequence[Path]:
 # eager is a bit more explicit for error handling
 
 
-class Scrobble(NamedTuple):
+@dataclass
+class Scrobble:
     raw: Json
 
-    # TODO mm, no timezone? hopefully it's UTC
     @property
-    def dt(self) -> datetime:
+    def dt(self) -> datetime_aware:
         ts = int(self.raw['date'])
         return datetime.fromtimestamp(ts, tz=timezone.utc)
 
@@ -63,7 +66,6 @@ class Scrobble(NamedTuple):
     def track(self) -> str:
         return f'{self.artist} — {self.name}'
 
-
     # TODO __repr__, __str__
     # TODO could also be nice to make generic? maybe even depending on eagerness
 
@@ -78,9 +80,6 @@ def scrobbles() -> Iterable[Scrobble]:
         yield Scrobble(raw=raw)
 
 
-from my.core import Stats, stat
-
-
 def stats() -> Stats:
     return stat(scrobbles)
 
@@ -89,8 +88,11 @@ def fill_influxdb() -> None:
     from my.core import influxdb
 
     # todo needs to be more automatic
-    sd = ({
-        'dt': x.dt,
-        'track': x.track,
-    } for x in scrobbles())
+    sd = (
+        {
+            'dt': x.dt,
+            'track': x.track,
+        }
+        for x in scrobbles()
+    )
     influxdb.fill(sd, measurement=__name__)
