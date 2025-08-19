b5923f77cd83068f22960fb038421d64ca581ca4
Dima Gerasimov <karlicoss@gmail.com>
2025-08-19 20:58:48 +0100

    my.gpslogger: minor cleanup and remove useless sort=False argument
    we're sorting files anyway, so shouldn't be necessary

--- src/my/location/gpslogger.py
+++ src/my/location/gpslogger.py
@@ -5,22 +5,8 @@ Parse [[https://github.com/mendhak/gpslogger][gpslogger]] .gpx (xml) files
 REQUIRES = ["gpxpy"]
 
 
-from dataclasses import dataclass
-
-from my.config import location
-from my.core import Paths
-
-
-@dataclass
-class config(location.gpslogger):
-    # path[s]/glob to the synced gpx (XML) files
-    export_path: Paths
-
-    # default accuracy for gpslogger
-    accuracy: float = 50.0
-
-
 from collections.abc import Iterator, Sequence
+from dataclasses import dataclass
 from datetime import datetime, timezone
 from itertools import chain
 from pathlib import Path
@@ -29,24 +15,41 @@ import gpxpy
 from gpxpy.gpx import GPXXMLSyntaxException
 from more_itertools import unique_everseen
 
-from my.core import Stats, make_logger
+from my.core import Paths, Stats, make_logger, stat
 from my.core.cachew import mcachew
 from my.core.common import get_files
 
 from .common import Location
 
+from my.config import location  # isort: skip
+
+
+@dataclass
+class config(location.gpslogger):
+    export_path: Paths
+    """
+    path[s]/glob to the synced gpx (XML) files
+    """
+
+    accuracy: float = 50.0
+    """
+    default accuracy for gpslogger
+    """
+
+
 logger = make_logger(__name__, level="warning")
 
+
 def _input_sort_key(path: Path) -> str:
+    # gpslogger files can optionally be prefixed by a device id,
+    # like b5760c66102a5269_20211214142156.gpx
     if "_" in path.name:
         return path.name.split("_", maxsplit=1)[1]
     return path.name
 
 
 def inputs() -> Sequence[Path]:
-    # gpslogger files can optionally be prefixed by a device id,
-    # like b5760c66102a5269_20211214142156.gpx
-    return sorted(get_files(config.export_path, glob="*.gpx", sort=False), key=_input_sort_key)
+    return sorted(get_files(config.export_path, glob="*.gpx"), key=_input_sort_key)
 
 
 def _cachew_depends_on() -> list[float]:
@@ -56,9 +59,7 @@ def _cachew_depends_on() -> list[float]:
 # TODO: could use a better cachew key/this has to recompute every file whenever the newest one changes
 @mcachew(depends_on=_cachew_depends_on, logger=logger)
 def locations() -> Iterator[Location]:
-    yield from unique_everseen(
-        chain(*map(_extract_locations, inputs())), key=lambda loc: loc.dt
-    )
+    yield from unique_everseen(chain(*map(_extract_locations, inputs())), key=lambda loc: loc.dt)
 
 
 def _extract_locations(path: Path) -> Iterator[Location]:
@@ -86,6 +87,4 @@ def _extract_locations(path: Path) -> Iterator[Location]:
 
 
 def stats() -> Stats:
-    from my.core import stat
-
     return {**stat(locations)}
